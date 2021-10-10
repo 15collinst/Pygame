253b3b490d0a9a0a97cd61201b10875362991e09
15collinst <15collinst@clevedonschool.org.uk>
2021-10-10 16:21:12 +0100

    coordinate system

--- button_main.py
+++ button_main.py
@@ -5,41 +5,51 @@ import button
 SCREEN_HEIGHT = 720
 SCREEN_WIDTH = 1280
 
-#position of the table
-PERIODIC_TABLE_X = (SCREEN_WIDTH - 1152) / 2 #centers the periodic table
-PERIODIC_TABLE_Y = 0
-
-#number of elements
-E = 18
-
+#pygame config
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Periodic Table')
 
-images = []
+#position of the table
+PERIODIC_TABLE_X = (SCREEN_WIDTH - 1152) / 2 #centers the periodic table - 1151 = width of periodic table
+PERIODIC_TABLE_Y = (SCREEN_HEIGHT - 500) #520 = height of periodic table
+
+#coordinates of table
+TABLE = [[1,18],
+[1,2,13,14,15,16,17,18],
+[1,2,13,14,15,16,17,18],
+[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18],
+[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18],
+[1,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18],
+[1,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]]
 
 #load element images, note: range = number of elements
+E = 88
+images = []
+
 for i in range(E):
 	i = str(i+1)
 	temp = '1' #I havent made the images yet
 	images.append(pygame.image.load('images/'+temp+'.png').convert_alpha())
 
-
 #create element instances, positioning elements
 elements = []
+p = 0
+row = 0
 
-x = PERIODIC_TABLE_X
-y = PERIODIC_TABLE_Y
-
-for i in range(E):
-	elements.append(button.Button(x, y, images[i]))
-	x = x + 65
+for r in TABLE:
+	row = row + 1
+	for c in r:
+	   x = PERIODIC_TABLE_X + ((c - 1) * 64)
+	   y = PERIODIC_TABLE_Y + ((row - 1) * 64)
+	   elements.append(button.Button(x, y, images[p]))
+	   p = p + 1
 
 #game loop
 run = True
 while run:
 
 	#nice blue background
-	screen.fill((202, 228, 241))
+	screen.fill((154, 199, 145))
 
 	#loops over all elements drawing them and checking if they have been clicked
 	for i in range(E):
@@ -56,4 +66,3 @@ while run:
 	pygame.display.update()
 
 pygame.quit()
-
