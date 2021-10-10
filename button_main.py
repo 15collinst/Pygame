54a107cf5a25bfab47a485c0f0c364e243c858f7
15collinst <15collinst@clevedonschool.org.uk>
2021-10-10 16:39:39 +0100

    setup file

--- button_main.py
+++ button_main.py
@@ -1,19 +1,19 @@
 import pygame
-import button
+import setup
 
-#create display window
+# create display window
 SCREEN_HEIGHT = 720
 SCREEN_WIDTH = 1280
 
-#pygame config
+# pygame config
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Periodic Table')
 
-#position of the table
+# position of the table
 PERIODIC_TABLE_X = (SCREEN_WIDTH - 1152) / 2 #centers the periodic table - 1151 = width of periodic table
 PERIODIC_TABLE_Y = (SCREEN_HEIGHT - 500) #520 = height of periodic table
 
-#coordinates of table
+# coordinates of table
 TABLE = [[1,18],
 [1,2,13,14,15,16,17,18],
 [1,2,13,14,15,16,17,18],
@@ -22,44 +22,30 @@ TABLE = [[1,18],
 [1,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18],
 [1,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]]
 
-#load element images, note: range = number of elements
-E = 88
+# creates instances of the images
 images = []
+setup.instance_imgs(images)
 
-for i in range(E):
-	i = str(i+1)
-	temp = '1' #I havent made the images yet
-	images.append(pygame.image.load('images/'+temp+'.png').convert_alpha())
-
-#create element instances, positioning elements
+# create element instances, positions elements
 elements = []
-p = 0
-row = 0
+setup.instance_elements(PERIODIC_TABLE_X, PERIODIC_TABLE_Y, TABLE, images, elements)
 
-for r in TABLE:
-	row = row + 1
-	for c in r:
-	   x = PERIODIC_TABLE_X + ((c - 1) * 64)
-	   y = PERIODIC_TABLE_Y + ((row - 1) * 64)
-	   elements.append(button.Button(x, y, images[p]))
-	   p = p + 1
-
-#game loop
+# game loop
 run = True
 while run:
 
-	#nice blue background
+	# nice blue background
 	screen.fill((154, 199, 145))
 
-	#loops over all elements drawing them and checking if they have been clicked
-	for i in range(E):
+	# loops over all elements drawing them and checking if they have been clicked
+	for i in range(88):
 		if elements[i].draw(screen):
 			i = str(i+1)
 			print('Click ['+i+']')
 
-	#event handler
+	# event handler
 	for event in pygame.event.get():
-		#quit game
+		# quit game
 		if event.type == pygame.QUIT:
 			run = False
 
---
+++ setup.py
@@ -0,0 +1,28 @@
+import pygame
+import button
+
+# creates instances of the images
+def instance_imgs(arr):
+
+    for i in range(88): # note: range = number of elements
+        i = str(i+1)
+        temp = '1' # note: I havent made the images yet
+        arr.append(pygame.image.load('images/'+temp+'.png').convert_alpha())
+
+    return arr
+
+# create element instances, positions elements
+def instance_elements(px, py, table, imgs, arr):
+    p = 0
+    row = 0
+    
+    for r in table:
+        row = row + 1
+        for c in r:
+            x = px + ((c - 1) * 64)
+            y = py + ((row - 1) * 64)
+            arr.append(button.Button(x, y, imgs[p]))
+            p = p + 1
+
+    return arr
+
