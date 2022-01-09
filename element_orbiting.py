74ba1087b82fdf748561d24a197ecd16ac26c336
15collinst <15collinst@clevedonschool.org.uk>
2022-01-09 11:50:23 +0000

    weird results

--- element_orbiting.py
+++ element_orbiting.py
@@ -5,6 +5,7 @@ pygame.init()
 
 bg = (255, 255, 255)
 orbit_col = (0, 0, 0)
+RED = (255,0,0)
 
 size = [1000, 1000]
 screen = pygame.display.set_mode(size)
@@ -19,8 +20,6 @@ ORBIT_RADIUS = 100
 ELECTRON_SIZE = 5
 ROTATION_SPEED = 1
 ELECTRONS = []
-
-number_of_electrons = 0
 
 class Electron():
     def __init__(self):
@@ -42,17 +41,23 @@ class Electron():
         cosine_of_orbit_degree = math.cos(self.angle * 0.0174532925)
         return my + (cosine_of_orbit_degree * ORBIT_RADIUS)
     
-    def get_angle(self, number_of_electrons):
-        mx,my = pygame.mouse.get_pos()
-        closest_distance = ORBIT_RADIUS * 2
-        for i in range(number_of_electrons):
-            x1 = self.get_x(mx)
-            y1 = self.get_y(my)
-            y2 = ELECTRONS[i].get_x()
-            x2 = ELECTRONS[i].get_y()
-            distance_to_neighbour = math.sqrt( ((x2 - x1) ** 2) + ((y2 - y1) ** 2))
-            if distance_to_neighbour < closest_distance:
-                closest_distance = distance_to_neighbour
+    def get_angle(self):
+        if len(ELECTRONS) == 1:
+            pass
+        else:
+            mx,my = pygame.mouse.get_pos()
+            closest_distance = ORBIT_RADIUS * 2
+            for i in range(len(ELECTRONS)):
+                x1 = self.get_x(mx)
+                y1 = self.get_y(my)
+                x2 = ELECTRONS[i].get_y(my)
+                y2 = ELECTRONS[i].get_x(mx)
+                distance_to_neighbour = math.sqrt( ((x2 - x1) ** 2) + ((y2 - y1) ** 2) )
+
+
+                if distance_to_neighbour < closest_distance:
+                    pygame.draw.line(screen, RED, [y2, x2], [y1, x1], 5)
+        
         
 
 
@@ -67,8 +72,10 @@ while run:
 
     screen.fill(bg)
 
-    for i in range(len(ELECTRONS) - 1):
+    for i in range(len(ELECTRONS)):
         ELECTRONS[i].draw()
+
+    ELECTRONS[0].get_angle()
 
     pygame.display.update()
 
@@ -76,4 +83,3 @@ while run:
 
 
 pygame.quit
-print(len(ELECTRONS))
