9968aa5c3939305f4ddf0090642865d16e53054e
15collinst <15collinst@clevedonschool.org.uk>
2022-02-06 19:30:30 +0000

    When element selected the sprite will spawn with electrons

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import image
 import math
 
 pygame.init()
+pygame.display.set_caption('Periodic Table')
 
 # create display window
 SCREEN_HEIGHT = 1024
@@ -12,7 +13,6 @@ SCREEN_WIDTH = 1440
 
 # pygame config
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Periodic Table')
 
 # position of the table
 PERIODIC_TABLE_X = 40
@@ -27,14 +27,6 @@ TABLE = [[1,18],
 [1,2,13,14,15,16,17,18],
 [1,2,13,14,15,16,17,18]]
 
-# creates instances of the images
-images = []
-setup.instance_imgs(images)
-
-# create element instances, positions elements
-elements = []
-setup.instance_elements(PERIODIC_TABLE_X, PERIODIC_TABLE_Y, TABLE, images, elements)
-
 # create key
 KEY_IMG = pygame.image.load("assets/misc/Key.svg").convert_alpha()
 KEY = image.Static_Image(KEY_X, KEY_Y, KEY_IMG, 1)
@@ -43,13 +35,19 @@ KEY = image.Static_Image(KEY_X, KEY_Y, KEY_IMG, 1)
 OTHER_ELEMENTS_IMG = pygame.image.load("assets/misc/Other_elements.svg").convert_alpha()
 OTHER_ELEMENTS = image.Static_Image(36, 625, OTHER_ELEMENTS_IMG, 1)
 
+# creates instances of the images
+IMAGES = []
+setup.instance_imgs(IMAGES)
+
+# create element instances, positions elements
+ELEMENTS = []
+setup.instance_elements(PERIODIC_TABLE_X, PERIODIC_TABLE_Y, TABLE, IMAGES, ELEMENTS)
+
 # game loop
 run = True
 spawn = False
 mouse = False
 clock = pygame.time.Clock()
-
-active_element = sprite.Sprite(15)
 
 while run:
 	# event handler
@@ -59,8 +57,8 @@ while run:
 		if event.type == pygame.MOUSEBUTTONUP:
 			# if player selects an element return element value
 			if my < 369 and spawn:
-				clicked = str(clicked)
-				active_element = sprite.Element(clicked)
+				active_element = sprite.Sprite(clicked)
+				print(f"Element {clicked} has been selected")
 			spawn = False
 			
 		# quit game
@@ -79,14 +77,14 @@ while run:
 
 	# loops over all elements drawing them and checking if they have been clicked
 	for i in range(18):
-		if elements[i].draw(SCREEN) and not spawn:
-			element_number = i + 1
-			spawn = True
+		if ELEMENTS[i].draw(SCREEN) and not spawn:
 			clicked = i + 1
+			active_element = sprite.Sprite(clicked)
+			spawn = True
 
 	#creates a sprite which tracks the mouse when you hold down
-	# if spawn:
-	active_element.draw(SCREEN, mx ,my)
+	if spawn:
+		active_element.draw(SCREEN, mx ,my)
 
 	pygame.display.update()
 
