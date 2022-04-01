89304e89353d41949e5efdfe9a7f40fba6b3ad8f
15collinst <15collinst@clevedonschool.org.uk>
2022-04-01 15:29:34 +0100

    calculate the number of sharing electrons

--- sprite.py
+++ sprite.py
@@ -1,35 +1,66 @@
+from xml.etree.ElementTree import ElementTree
 import pygame
 import electron
 
 class Sprite():
     def __init__(element, atomic_number):
+        # encapsulate the atomic number
         element.atomic_number = atomic_number
-        element.angle = 0
-        num_of_electrons = atomic_number
 
+        # set the elements starting angle and shared electrons
+        element.angle = 0
+        element.shared_electrons = 0
+
+        # set the symbol of the electron
+        elements = ["H", "He","Li","Be","B","C","N","O","F","Ne","Na","Mg","Al","Si","P","S","Cl","Ar"]
+        font = pygame.font.Font("assets/sprites/Roboto-Regular.ttf", 32)
+        element.symbol = font.render(elements[atomic_number -1], True, (255,255,255))
+
+
+        # deduce the number of outer shell electrons
+        num_of_electrons = atomic_number
         if atomic_number > 2:
             num_of_electrons = atomic_number - 2
         if atomic_number > 10:
             num_of_electrons = atomic_number - 10
 
+
+        # creates all the electrons for this element
         electrons = []
         for i in range(num_of_electrons):
             electrons.append(electron.Electron())
         element.electrons = electrons
-        
-    def draw(element, SCREEN, mx ,my):
-        elements = ["H", "He","Li","Be","B","C","N","O","F","Ne","Na","Mg","Al","Si","P","S","Cl","Ar"]
-        symbol = elements[element.atomic_number -1]
-        font = pygame.font.Font("assets/sprites/Roboto-Regular.ttf", 32)
-        img = font.render(symbol, True, (255,255,255))
-        sprite_centre_x = mx - img.get_width() / 2
-        sprite_centre_y = my - img.get_height() / 2  
-        SCREEN.blit(img, (sprite_centre_x, sprite_centre_y))
 
+        print(element.get_sharing_electrons())
+        
+    # draws the element on the screen
+    def draw(element, SCREEN, x ,y):
+        sprite_centre_x = x - element.symbol.get_width() / 2
+        sprite_centre_y = y - element.symbol.get_height() / 2  
+        SCREEN.blit(element.symbol, (sprite_centre_x, sprite_centre_y))
+
+        # draw all of the electrons going around the element
         for i in range(len(element.electrons)):
             rotation = i * (360 / len(element.electrons))
-            element.electrons[i].draw(SCREEN, element.angle+rotation, mx, my)
+            element.electrons[i].draw(SCREEN, element.angle+rotation, x, y)
 
         element.angle += 1
+
+
+    def get_sharing_electrons(element):
+        num_of_outer_electrons = len(element.electrons) + element.shared_electrons
+
+        # exeption for H and He which are trying to achive an outer shell of 2
+        if element.atomic_number <= 2:
+            # element has a full outer shell and so no sharing electrons
+            if num_of_outer_electrons == 2: return 0
+            else: return num_of_outer_electrons
+        else:
+            # element has a full outer shell and so no sharing electrons
+            if num_of_outer_electrons == 8: return 0
+            else: return num_of_outer_electrons
+
+
+
 
 
